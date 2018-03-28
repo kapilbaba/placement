6c935533a22121a01014671438e0cf303bad0f6c
kapilanand <kapil.anand.5030927@gmail.com>
2018-03-28 13:16:16 +0530

    certain change

--- core/database/connection_db.php
+++ core/database/connection_db.php
@@ -6,6 +6,6 @@
  * Time: 09:58
  */
 $db_error ="sorry for the in convinence";
-$conn = mysqli_connect("localhost:3306", "root", "Akosta@123","akosta" )
+$conn = mysqli_connect("localhost:3306", "root", "root","akosta" )
 or die($db_error);
 ?>
