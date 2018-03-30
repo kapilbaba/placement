60b4c2436cdc4e03c1119b8e912e521783efc66b
kapilanand <kapil.anand.5030927@gmail.com>
2018-03-30 14:05:13 +0530

    new coomit asdknklsad

--- enquiry.php
+++ enquiry.php
@@ -10,6 +10,9 @@ body{
     background-repeat: no-repeat;
     background-size: cover
 }
+    p{
+        margin: 10px;
+    }
 </style>
 <div id="page">
     <?php include "layout/header.php"; ?>
@@ -17,42 +20,44 @@ body{
     <section class="contact-us">
 
         <div class="form_page">
-            <form action="mail.php" method="post">
 
-<div class="col-md-12">
+            <div id="contact-page" class="container">
+                <div class="bg" style="margin-top: 5%; margin-bottom: 5%">
+                    <div class="row">
+                        <div class="col-md-8 col-sm-6">
+                            <h2 class="title text-center">Contact <strong>Us</strong></h2>
+                            <BR>
+                            <div id="gmap" class="contact-map">
+                                <iframe width="100%" height="250px" frameborder="3" style="border:0" align="center" src="https://www.google.com/maps/embed/v1/search?key=AIzaSyA6fl3wJi1-Zsc1YpE8ZDqbxrlpJrULrN0&amp;q=c218+sector-63+noida" allowfullscreen="">
+                                </iframe>
+                            </div>
+                        </div>
 
-</div>
-                <div class="col-md-12 col-sm-6 col-xs-12">
 
-                    <div class="form-group">
-                        <label for="name"> name</label>
-                        <input type="text" name="name" class="form-control" id="name">
+                        <div class="col-sm-6 col-md-4">
+                            <div class="contact-info">
+                                <h2 class="title text-center">Contact Info</h2>
+                                <BR>
+                                <address>
+                                    <p>Akosta placement</p>
+                                    <p style="padding: 2px; border: 1px solid">company information Pvt. Ltd.
+                                        <br>
+                                        C-218, Sector-63, Noida, U.P.</p>
+
+                                    <p>Support Email</p>
+                                    <p style="padding: 2px; border: 1px solid">contact@gakostaplacement.com</p>
+                                    <P>Phone</P>
+                                    <p style="padding: 2px; border: 1px solid">+911204108678 </p>
+                                </address>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-12 col-sm-6 col-xs-12">
-                    <div class="form-group">
-                        <label for="email">email</label>
-                        <input type="email" name="email" class="form-control" id="email">
-                    </div>
-                </div>
-
-                <div class="col-md-12 col-sm-6 col-xs-12">
-                    <div class="form-group">
-                        <label for="subject">subject</label>
-                        <input type="text" name="subject" class="form-control" id="subject">
-                    </div>
-                </div>
-
-                <div class="col-md-12 col-sm-6 col-xs-12">
-                    <textarea id="message" name="message" placeholder="Write something.." cols="75" rows="5"></textarea>
-                </div>
-                <p style="text-align: center">
+            </div>
 
 
-                    <button type="submit" class="button1">send</button>
-                </p>
 
-            </form>
         </div>
     </section>
 
