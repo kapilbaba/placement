d65a3e3db75c17c2e55d2b28d9d31cdb01851818
kapilanand <kapil.anand.5030927@gmail.com>
2018-03-31 14:42:00 +0530

    new coomit asdknklsad

--- mail.php
+++ mail.php
@@ -5,15 +5,51 @@
  * Date: 28/03/18
  * Time: 08:45
  */
+
+$apikey = '8ce09a535b51cf8884ea53f9f1bbe26eeeb995bc';
+
 $name =$_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message =$_POST['message'];
 
-$mailto ="contact@akostaplacement.com";
-$header ="from:" .$email;
-$txt ="you recive a text from ".$name."./n/n".$message;
 
-mail($mailto, $subject, $txt ,$header);
+$curl = curl_init();
+
+$json = [
+    'options'=>['sandbox'=>true],
+    'content'=>[
+        'from'=>'contact@akostaplacment.com',
+        'subject'=>$subject,
+        'text'=>$message
+    ],
+    'recipients'=>[
+        ['address'=>'contact@akostaplacement.com']
+    ]
+];
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.sparkpost.com/api/v1/transmissions",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => json_encode($json),
+    CURLOPT_HTTPHEADER => array(
+        "authorization: ".$apikey,
+        "cache-control: no-cache",
+        "content-type: application/json"
+    ),
+));
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
+}
 header("index.php?mailsend");
 
