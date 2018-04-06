f2ec475658a8a577b876ca3d0a6a4750fce2eaf1
kapilanand <kapil.anand.5030927@gmail.com>
2018-04-06 14:29:27 +0530

    new t

--- layout/mobile-footer.php
+++ layout/mobile-footer.php
@@ -8,7 +8,7 @@
 
 <style>
     .footer {
-
+        position: absolute;
         font-family: inherit;
         left: 0;
         bottom: 0;
@@ -20,5 +20,7 @@
 </style>
 
 <div class="footer">
-  <p style="text-align: center"><a style="color: white ; text-decoration: none" href="/candidate-signup">Sign-up</a>&nbsp;&nbsp;|&nbsp;&nbsp; <a style="color: white; text-decoration: none" href="/candidate-login">Login</a>&nbsp;&nbsp;|&nbsp;&nbsp; <a style="color: white; text-decoration: none;" href="/user-jobs">Jobs</a></p>
+    <p style="text-align: center"><a style="color: white ; text-decoration: none" href="/candidate-signup">Sign-up</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+        <a style="color: white; text-decoration: none" href="/candidate-login">Login</a>&nbsp;&nbsp;|&nbsp;&nbsp; <a
+                style="color: white; text-decoration: none;" href="/user-jobs">Jobs</a></p>
 </div>
