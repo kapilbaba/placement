e8465de1f05833b955d134099322b04bdd77bc3e
kapilanand <kapil.anand.5030927@gmail.com>
2018-04-06 17:20:46 +0530

    new t

--- layout/mobile-footer.php
+++ layout/mobile-footer.php
@@ -5,22 +5,59 @@
  * Date: 06/04/18
  * Time: 09:24
  */ ?>
-
 <style>
-    .footer {
-        position: absolute;
-        font-family: inherit;
-        left: 0;
+
+
+
+    .footer-mobile {
+        background: #000;
+        position: static;
         bottom: 0;
-        width: 100%;
-        background-color: #2d668f;
-        color: white;
-        text-align: center;
+        width: 100%
+    }
+
+    .support-email:before {
+        background-position: -18px -162px;
+        width: 15px
+    }
+
+    .support-email:before {
+        vertical-align: middle;
+        margin-right: 5px;
+        height: 12px
+    }
+
+    .footer-bdr-btm {
+        border-bottom: 1px dotted #333
+    }
+
+    .footer-btm-bar {
+        background-color: #0c0c0c;
+        line-height: 40px
+    }
+
+    .footer-link a {
+        display: inline-block;
+        margin: 0 10px;
+        text-decoration: none;
+        color: #fff
+    }
+
+    .text-deco-none {
+        text-decoration: none
+    }
+
+    .clr-777 {
+        color: #777
     }
 </style>
-
-<div class="footer">
-    <p style="text-align: center"><a style="color: white ; text-decoration: none" href="/candidate-signup">Sign-up</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-        <a style="color: white; text-decoration: none" href="/candidate-login">Login</a>&nbsp;&nbsp;|&nbsp;&nbsp; <a
-                style="color: white; text-decoration: none;" href="/user-jobs">Jobs</a></p>
+<div class="footer-mobile clr-fff mt10">
+    <div class="p10 fs13 footer-bdr-btm text-center footer-link">
+        <a href="/candidate-signup">register</a>
+        <a href="/contact">Contact Us</a>
+        <a href="/track-application">Track applicant</a>
+    </div>
+    <div class="footer-btm-bar clr-777 text-center fs13">
+        ©2018 Akostaplacement Pvt.Ltd.
+    </div>
 </div>
