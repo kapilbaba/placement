da45a8aa2ce2a35748d7b71070ffd481b89adb9c
kapilanand <kapil.anand.5030927@gmail.com>
2018-04-17 14:29:18 +0530

    new t

--- core/database/connection_db.php
+++ core/database/connection_db.php
@@ -6,7 +6,7 @@
  * Time: 09:58
  */
 $db_error ="sorry for the in convinence";
-$conn = mysqli_connect("localhost:3306", "root", "Akosta@123","akosta" )
+$conn = mysqli_connect("localhost:3306", "root", "root","akosta" )
 or die($db_error);
 
 
