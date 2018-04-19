930ebc7fac0bdcb922c563c7db6c10310d6c0638
kapilanand <kapil.anand.5030927@gmail.com>
2018-04-20 01:05:07 +0530

    new t

--- contract-staffing-service.php
+++ contract-staffing-service.php
@@ -70,14 +70,7 @@
                                     <div class="tab-content">
                                         <div role="tabpanel" class="tab-pane fade active in" >
                                             <div class="about_company">
-                                                <div class="row business_divider_img">
-                                                    <div class="col-md-12 col-xs-12 col-sm-12">
-                                                        <div class="our_about_image b_left_img">
-                                                            <img src="images/about-company-img4.jpg" alt="">
-                                                        </div>
-                                                    </div>
-
-                                                </div>
+                                               
                                                 <div class="section_title">
                                                     <h2>We give best advice for your business</h2>
                                                 </div>
