f889ac3280ecda6ce75a2eedaaff90176670a625
kapilanand <kapil.anand.5030927@gmail.com>
2018-05-14 15:00:53 +0530

    new

--- layout/core/script.php
+++ layout/core/script.php
@@ -1,5 +1,7 @@
+
 <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script><script type="text/javascript" src="/javascript/bootstrap-dropdownhover.min.js"></script>
+<script type="text/javascript" src="/javascript/jquery-scrolltofixed-min.js"></script>
 <script  type="text/javascript" src="/javascript/bootstrap-datepicker.min.js"></script>
 <script type="text/javascript" src="/javascript/lightbox.min.js"></script>
 <script type="text/javascript" src="/javascript/owl.carousel.min.js"></script>
@@ -10,10 +12,9 @@
 <script type="text/javascript" src="/javascript/wow.min.js"></script>
 <script  type="text/javascript" src="/javascript/imagesloaded.min.js"></script>
 <script type="text/javascript" src="/javascript/jquery.filterizr.min.js"></script>
-
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="/bootstrap/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
 
 
 <script src="/javascript/main.js"></script>
