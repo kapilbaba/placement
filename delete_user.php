b989bea1b21e90d458c3d4ba1d3613029ce117df
kapilanand <kapil.anand.5030927@gmail.com>
2018-05-16 19:32:43 +0530

    new

--- delete_user.php
+++ delete_user.php
@@ -15,7 +15,7 @@ include "core/database/connection_db.php";
 $sql = "DELETE FROM users WHERE id='$id'";
 
 if ($conn->query($sql) === TRUE) {
-   header("location:alluser.php");
+   header("location:allusers.php");
 $_SESSION['user_deleted'] ="User is deleted";
 } else {
     echo "Error deleting record: " . $conn->error;
